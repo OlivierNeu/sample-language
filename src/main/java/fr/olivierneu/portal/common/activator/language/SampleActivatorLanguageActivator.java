7c79cc913a9c1cd12a009abdd8ae24c08889a8ee
Olivier Neu <olivierneu.one@gmail.com>
2019-09-30 22:58:17 -0400

    First init Rescources

---
+++ src/main/java/fr/olivierneu/portal/common/activator/language/SampleActivatorLanguageActivator.java
@@ -0,0 +1,19 @@
+package fr.olivierneu.portal.common.activator.language;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * @author olivierneu
+ */
+public class SampleActivatorLanguageActivator implements BundleActivator {
+
+	@Override
+	public void start(BundleContext bundleContext) throws Exception {
+	}
+
+	@Override
+	public void stop(BundleContext bundleContext) throws Exception {
+	}
+
+}
---
+++ src/main/java/fr/olivierneu/portal/common/activator/language/frFRResourceBundle.java
@@ -0,0 +1,27 @@
+package fr.olivierneu.portal.common.activator.language;
+
+import com.liferay.portal.kernel.language.UTF8Control;
+import org.osgi.service.component.annotations.Component;
+
+import java.util.Enumeration;
+import java.util.ResourceBundle;
+
+@Component(
+        property = {"language.id=fr_FR"},
+        service = ResourceBundle.class
+)
+public class frFRResourceBundle extends ResourceBundle {
+
+    @Override
+    protected Object handleGetObject(String key) {
+        return _resourceBundle.getObject(key);
+    }
+
+    @Override
+    public Enumeration<String> getKeys() {
+        return _resourceBundle.getKeys();
+    }
+
+    private final ResourceBundle _resourceBundle = ResourceBundle.getBundle(
+            "content.Language", UTF8Control.INSTANCE);
+}
